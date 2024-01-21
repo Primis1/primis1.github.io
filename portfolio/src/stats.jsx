d2b1cbd6b9981593db980610e721be3c82766ffd
Olek <149747215+Primis1@users.noreply.github.com>
2024-01-20 23:51:31 -0500

    YES IT FKN WORKS

---
+++ portfolio/src/stats.jsx
@@ -0,0 +1,13 @@
+import './data.css'
+// import { useState, useEffect } from 'react'
+
+export function Stats() {
+    
+    
+    return (
+        <div className="time__wrapper">
+
+        </div>
+
+    )
+}
--- portfolio/src/wheather.jsx
+++ portfolio/src/wheather.jsx
@@ -1,5 +1,6 @@
 import {Data} from './date.jsx'
 import { Th } from './th.jsx'
+import { Stats } from './stats.jsx'
 import './wheather.css'
 
 export function Wheather() {
@@ -8,8 +9,8 @@ export function Wheather() {
             <div className='container'>
                 <div className="app__wrapper"> 
                     <Data /> 
+                    <Stats />
                     <Th />
-                   
                  </div>
                 
                 
