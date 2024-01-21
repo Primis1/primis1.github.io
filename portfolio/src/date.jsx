7f498f41bc09dbe0e1f5ffc090c83c7bc1da87a1
Olek <149747215+Primis1@users.noreply.github.com>
2024-01-20 23:30:23 -0500

    changed api to https

--- portfolio/src/date.jsx
+++ portfolio/src/date.jsx
@@ -5,7 +5,7 @@ export function Data() {
     const [ miss, setMiss ] = useState([])
 
     useEffect (() => {
-        fetch('http://api.weatherapi.com/v1/current.json?key=dcf27b0a701c4de5907162206241601&q=Mississauga&aqi=no')
+        fetch('https://api.weatherapi.com/v1/current.json?key=dcf27b0a701c4de5907162206241601&q=Mississauga&aqi=no')
         .then(response => response.json())
         .then(miss => setMiss(miss))
         .catch(error => console.error('Error:', error));
--- portfolio/src/th.jsx
+++ portfolio/src/th.jsx
@@ -8,7 +8,7 @@ import './data.css'
     const [ miss, setMiss ] = useState([])
 
     useEffect (() => {
-        fetch('http://api.weatherapi.com/v1/current.json?key=dcf27b0a701c4de5907162206241601&q=Mississauga&aqi=no')
+      fetch('https://api.weatherapi.com/v1/current.json?key=dcf27b0a701c4de5907162206241601&q=Mississauga&aqi=no')
         .then(response => response.json())
         .then(miss => setMiss(miss))
         .catch(error => console.error('Error:', error));
